2a50bca49574a27f1ab6da1e8e0e0037342ebd0b
BOUYAAYMANE <aymanebouya22@gmail.com>
2025-04-04 15:33:10 +0000

    Arrêt du suivi du dossier src/redux

--- src/redux/store/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import { moviesReducer } from "../reducer/moviesReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { thunk } from "redux-thunk";
-// 1- create store
-// middlewre :center bin action et reducer
-// thunk : kat3wed async await
-// 1-Middleware  partie 1
-export const store = createStore(moviesReducer,applyMiddleware(thunk))
--- src/redux/type/moviesType.js
+++
@@ -1,4 +0,0 @@
-export const ALLMOVIES = "ALLMOVIES"
-// export const SERACHMOVIES = "SERACHMOVIES"
-export const MoviesApi = "https://api.themoviedb.org/3/movie/popular?api_key=42241d84c6d646fdf31d79a02117c483&language=ar"
-// export const SerchApi =
